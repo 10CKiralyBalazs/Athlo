fa82cda198b8b0c08f0e6e138f5e1b135b138cee
Darkside231 <c0kirbal@vasvari.org>
2026-01-16 08:28:53 +0100

    ed

---
+++ backend/Entity/Edzok.cs
@@ -0,0 +1,27 @@
+﻿namespace MyApp.Backend.Entity
+{
+    public class Edzok
+    {
+        public string Name { get; set; } = string.Empty;
+
+
+        public int Age { get; set; }
+
+
+        public string Egyesulet { get; set; } = string.Empty;
+
+        public string Sportoloi { get; set; } = string.Empty;
+
+        public Edzok() { }
+
+        public Edzok(string name, int age, string egyesulet, string sportoloi)
+        {
+            Name = name;
+            Age = age;
+            Egyesulet = egyesulet;
+            Sportoloi = sportoloi;
+        }
+
+        public override string ToString() => $"{Name} – {Age} - {Egyesulet} - {Sportoloi})";
+    }
+}
