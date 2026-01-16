c03df97d91f604cd7555fae9e6d5e3d62afcd3ba
Darkside231 <c0kirbal@vasvari.org>
2026-01-16 08:34:12 +0100

    rajongok

---
+++ backend/Entity/Rajongok.cs
@@ -0,0 +1,24 @@
+﻿namespace MyApp.Backend.Entity
+{
+    public class Rajongok
+    {
+        public string Name { get; set; } = string.Empty;
+
+
+        public int Age { get; set; }
+
+
+        public string Sportag { get; set; } = string.Empty;
+
+        public Rajongok() { }
+
+        public Rajongok(string name, int age, string sportag)
+        {
+            Name = name;
+            Age = age;
+            Sportag = sportag;
+        }
+
+        public override string ToString() => $"{Name} - {Age} – {Sportag}";
+    }
+}
