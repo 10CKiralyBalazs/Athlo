b2fee9bb80f06918e3d1fc6f299272de6f78c16b
Darkside231 <c0kirbal@vasvari.org>
2026-01-16 08:28:14 +0100

    edzok

---
+++ shared/MyApp.Shared/Models/Edzok.cs
@@ -0,0 +1,31 @@
+namespace MyApp.Shared.Models
+{
+    /// <summary>
+    /// Tantárgy adatait tároló osztály.
+    /// </summary>
+    public class Edzok
+    {
+        
+        public string Name { get; set; } = string.Empty;
+
+        
+        public int Age { get; set; }
+
+       
+        public string Egyesulet { get; set; } = string.Empty;
+
+        public string Sportoloi { get; set; } = string.Empty; 
+
+        public Edzok() { }
+
+        public Edzok(string name, int age, string egyesulet, string sportoloi)
+        {
+            Name = name;
+            Age = age ;
+            Egyesulet = egyesulet;
+            Sportoloi = sportoloi;
+        }
+
+        public override string ToString() => $"{Name} – {Age} - {Egyesulet} - {Sportoloi})";
+    }
+}
